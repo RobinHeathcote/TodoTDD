a643de17924a15cc84802e720243638b0533320d
naridas <gregsparta@gmail.com>
2016-06-02 16:30:56 +0100

    part 12 Testing Factories

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -1,14 +1,14 @@
-describe("toDoApp", function() {
+describe("Todos tracker", function() {
   it("should get home page title", function() {
     browser.get('/');
-    expect(browser.getTitle()).toEqual("Todos App");
+    expect(browser.getTitle()).toMatch("Todos App");
   });
 
   it('has several ToDos', function() {
     browser.get('/');
     var todos = $$('#todos p');
-    expect(todos.first().getText()).toEqual('ToDo1: completed');
-    expect(todos.last().getText()).toEqual('ToDo2: not completed');
+    expect(todos.first().getText()).toMatch('ToDo1: completed');
+    expect(todos.last().getText()).toMatch('ToDo2: not completed');
   });
 
 
@@ -19,12 +19,13 @@ describe("toDoApp", function() {
     $('#add-todo').click();
 
     var todo = $$('#todos p').last().getText();
-    expect(todo).toEqual('NewToDo: not completed');
+    expect(todo).toMatch('NewToDo: not completed');
   });
 
   it('can remove a ToDo', function() {
     browser.get('/');
     var todos = $$('#todos p');
+    var initialCount = todos.count();
 
     $('#remove-todo').click();
 
@@ -32,5 +33,13 @@ describe("toDoApp", function() {
     // The solution is actually surprisingly complex so we've kept in the magic
     // number for simplicity's sake
     expect(todos.count()).toEqual(1);
+  });
+
+  it('can mark a ToDo as complete', function(){
+    browser.get('/');
+    var todo = $$('#todos p').last();
+    todo.element(by.css('.complete')).click();
+
+    expect(todo.getText()).toMatch("ToDo2: completed");
   });
 });
--- test/unit/ToDoController.spec.js
+++ test/unit/ToDoController.spec.js
@@ -1,32 +1,38 @@
 describe('ToDoController', function() {
   beforeEach(module('toDoApp'));
 
-  var ctrl;
+  var ctrl, ToDoFactory;
 
-  beforeEach(inject(function($controller) {
+  beforeEach(inject(function($controller, _ToDoFactory_) {
     ctrl = $controller('ToDoController');
+    // This has underscores around it so we can then set it to a variable called
+    // ToDoFactory, see:
+    // https://docs.angularjs.org/api/ngMock/function/angular.mock.inject
+    ToDoFactory = _ToDoFactory_;
   }));
 
   it('initialises with several todos', function() {
-    var todos = [
-      {text: "ToDo1", completed: true},
-      {text: "ToDo2", completed: false}
-    ];
+    // Create todos now using the factory
+    var todo1 = new ToDoFactory("ToDo1", true);
+    var todo2 = new ToDoFactory("ToDo2", false);
 
-    expect(ctrl.todos).toEqual(todos);
+    expect(ctrl.todos).toEqual([todo1, todo2]);
   });
 
   it('adds a new todo', function() {
-    ctrl.addToDo('NewTodo');
+    ctrl.addToDo('NewToDo');
 
-    var todo = {text: "NewTodo", completed: false};
+    // Similarly this now uses a factory
+    var todo = new ToDoFactory("NewToDo");
     expect(ctrl.todos.pop()).toEqual(todo);
   });
+
   it('removes the last todo', function() {
-    var initialCount = ctrl.todos.length;
+    initialCount = ctrl.todos.length;
 
     ctrl.removeToDo();
 
     expect(ctrl.todos.length).toEqual(initialCount - 1);
   });
+
 });
