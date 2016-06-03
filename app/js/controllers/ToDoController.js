a1f3990e717dcc14ab4fcdf3d3628631437b3c81
naridas <gregsparta@gmail.com>
2016-06-03 12:36:12 +0100

    part14 done but no tests

--- app/js/controllers/ToDoController.js
+++ app/js/controllers/ToDoController.js
@@ -12,4 +12,18 @@ toDoApp.controller('ToDoController', ['ToDoService', 'ToDoFactory', function(ToD
   self.removeToDo = function() {
     self.todos.pop();
   };
+
+  angular.element(document).ready(function () {
+    self.toDosCount = function() {
+      return self.todos.length;
+    };
+  });
+
+  self.archive = function() {
+    var oldTodos = self.todos;
+    self.todos = [];
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.completed) self.todos.push(todo);
+    });
+  };
 }]);
