03654bff3b1306c42a014bd5a77c670932491505
naridas <gregsparta@gmail.com>
2016-06-03 11:22:05 +0100

    part13 testing service

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,5 +1,14 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['e2e/*.js'],
-  baseUrl: 'http://localhost:8080'
-}
+  baseUrl: 'http://localhost:8080',
+  onPrepare: function(){
+    var SpecReporter = require('jasmine-spec-reporter');
+    // add jasmine spec reporter
+    jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'all'}));
+    require('protractor-http-mock').config = {
+      rootDirectory: process.cwd(),
+      protractorConfig: 'test/protractor.conf.js'
+    };
+  }
+};
--- test/unit/ToDoController.spec.js
+++ test/unit/ToDoController.spec.js
@@ -1,14 +1,18 @@
 describe('ToDoController', function() {
   beforeEach(module('toDoApp'));
 
-  var ctrl, ToDoFactory;
+  var ctrl, httpBackend, ToDoFactory;
+  var toDos = [{text: "ToDo1", completed: true}, {text: "ToDo2", completed: false}];
 
-  beforeEach(inject(function($controller, _ToDoFactory_) {
+  beforeEach(inject(function($httpBackend, $controller, _ToDoFactory_) {
     ctrl = $controller('ToDoController');
     // This has underscores around it so we can then set it to a variable called
     // ToDoFactory, see:
     // https://docs.angularjs.org/api/ngMock/function/angular.mock.inject
     ToDoFactory = _ToDoFactory_;
+    httpBackend = $httpBackend;
+    httpBackend.expectGET("http://quiet-beach-24792.herokuapp.com/todos.json").respond(toDos);
+    httpBackend.flush();
   }));
 
   it('initialises with several todos', function() {
